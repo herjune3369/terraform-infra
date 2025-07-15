db9c2f87446223bab84f0e86300ac879e54ff82d
JUN HEO <junheo@JUNui-MacBookPro.local>
2025-07-15 09:32:45 +0900

    go

--- devsecops-k8s/terraform/backend.tf
+++ devsecops-k8s/terraform/backend.tf
@@ -6,4 +6,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "herjune-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
 }

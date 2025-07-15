059e51a38526791a8bdf245db6909db2bd438983
JUN HEO <junheo@JUNui-MacBookPro.local>
2025-07-15 13:43:49 +0900

    Create devops-1 branch with only devops-1 project

--- devsecops-2/terraform/save_outputs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create ansible directory if it doesn't exist
-mkdir -p ../ansible
-
-# Save terraform outputs to JSON file
-terraform output -json > ../ansible/terraform_outputs.json
-
-echo "✅ Terraform outputs saved to ../ansible/terraform_outputs.json" 

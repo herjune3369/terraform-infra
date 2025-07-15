d541d110c61abaadf09e88ab21472403dd311968
JUN HEO <junheo@JUNui-MacBookPro.local>
2025-07-15 09:49:44 +0900

    sfa

--- devsecops-k8s/terraform/backend.tf
+++ devsecops-k8s/terraform/backend.tf
@@ -1,17 +1,16 @@
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     bucket         = "herjune-terraform-state"
     key            = "terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
